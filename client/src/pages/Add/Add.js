8998e49889773b3849b3bee58ac0700b5d0d41d9
Michael Ochoa <jdmmike@me.com>
2018-09-10 12:10:55 -0700

    change add page and delete unused component

--- client/src/pages/Add/Add.js
+++ client/src/pages/Add/Add.js
@@ -24,27 +24,32 @@ class Add extends Component {
   render() {
     return (
       <div>
-      <Navbar />
-      <div className="container">
-        <br></br>
-        <h2>Add Recipe</h2>
-        <form>
-          <div className="form-group">
-            <label className="label" for="exampleInputEmail1">Recipe Name</label>
-            <input type="text" className="form-control" name="recipeName" placeholder="Fish Tacos" />
+        <Navbar />
+        <div className="container">
+          <br></br>
+          <div className="add">Add a<span> </span>
+            <span className="google-logo"><span className="google-R">R</span>
+              <span className="google-e">e</span><span className="google-o2">c</span>
+              <span className="google-R">i</span><span className="google-l">p</span>
+              <span className="google-o1">e</span></span></div>
+          <br></br>
+          <form>
+            <div className="form-group">
+              <label className="label" for="exampleInputEmail1">Recipe Name</label>
+              <input type="text" className="form-control" name="recipeName" placeholder="Fish Tacos" />
 
-          </div>
-          <div className="form-group">
-            <label className="label" for="ingredients">Ingredients</label>
-            <input type="text" className="form-control" name="ingredients" placeholder="Fish, Tortilla, etc.." />
-          </div>
-          <div className="form-group">
-            <label  className="form-check-label" for="exampleCheck1">Steps</label>
-            <input type="text" className="form-control" name="steps" placeholder="Cook fish, place fish in tortilla, eat" />
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-      </div>
+            </div>
+            <div className="form-group">
+              <label className="label" for="ingredients">Ingredients</label>
+              <input type="text" className="form-control" name="ingredients" placeholder="Fish, Tortilla, etc.." />
+            </div>
+            <div className="form-group">
+              <label className="form-check-label" for="exampleCheck1">Steps</label>
+              <input type="text" className="form-control" name="steps" placeholder="Cook fish, place fish in tortilla, eat" />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        </div>
       </div>
     )
   }
