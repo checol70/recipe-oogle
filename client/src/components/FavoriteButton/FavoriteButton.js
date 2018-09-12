375bfd5f402c05abaa242161028526838ba8ccec
Michael Ochoa <jdmmike@me.com>
2018-09-12 11:31:58 -0700

    ionicons add

--- client/src/components/FavoriteButton/FavoriteButton.js
+++ client/src/components/FavoriteButton/FavoriteButton.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const FavoriteButton = props =>(
     <button className= "favorite-button"{...props}>
-    {props.favorite? "★" : "✰"} 
+    {props.favorite? <ion-icon name="star"></ion-icon> : <ion-icon name="star-outline"></ion-icon>} 
     </button>
 )
 export default FavoriteButton;
